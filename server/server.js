fea90970e6db32cb2bd7660f9e13d43a64570a63
Luca Mitchell <lucajmitchell@gmail.com>
2025-02-12 16:27:05 +0000

    Clean slate (attempt 2)

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
-
-app.listen(5000, () => {console.log('Server is running on port 5000')});
